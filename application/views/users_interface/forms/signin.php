a0a4cf374be344b2c74d79eae895184bd5dd0755
Владимир <vkharseev@gmail.com>
2015-05-27 13:03:42 +0300

    POST login (авторизация)

--- application/views/users_interface/forms/signin.php
+++ application/views/users_interface/forms/signin.php
@@ -5,7 +5,8 @@
 		<div class="login-popup-enter">
 			<a href="#" class="footer-link no-clickable a-forgot-pass"><?=$this->localization->getLocalPlaceholder('signin','login_forgot')?></a><br>
 			<div class="div-form-operation">
-				<button class="red-button begin-button btn-submit btn-locked" id="auth-enter"><?=$this->localization->getLocalPlaceholder('signin','login_account')?></button><br>
+				<!--<button class="red-button begin-button btn-submit btn-locked" id="auth-enter"><?=$this->localization->getLocalPlaceholder('signin','login_account')?></button><br>-->
+				<button class="red-button begin-button btn-locked"><?=$this->localization->getLocalPlaceholder('signin','login_account')?></button><br>
 			</div>
 			<a href="<?=site_url('registering');?>" class="footer-link"><?=$this->localization->getLocalPlaceholder('signin','login_reg')?></a>
 		</div>
